34089dbb7376c682bbac1d2eaa3de43d37675c51
DD <130784367+DevDuckling@users.noreply.github.com>
2023-04-22 22:15:34 +0530

    add math object

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ const maxAge = 28;
 // immutable
 
 var minAge = 18;
+
+console.log(Math.random()); 
+// creates number between 0 and 1
+
+console.log(Math.random() * 6)
+// would give number between 0 and 5 if we want 6 in it then add + 1 to it
+
+console.log((Math.random() * 6) + 1);
+// to remove decimal we use Math.floor
+
+console.log(Math.floor(Math.random() * 6) + 1);
+// result is including 1 to 6 
