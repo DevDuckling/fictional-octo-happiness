0b82ee32de893b0ea6877ff3059507042b7575d0
DD <130784367+DevDuckling@users.noreply.github.com>
2023-05-05 19:05:47 +0530

    comment out dom...
    when in need just, undo comment and access

---
+++ dom/dom.js
@@ -0,0 +1,42 @@
+// const container = document.querySelector('.container');
+// const parent = document.querySelector('#parent');
+// const parentList = document.querySelectorAll('.parentList');
+// const colors = ['green', 'blue', 'cyan', 'pink'];
+
+// // ---------------------ITERATING over li------------------------------
+// // for(let li of parentList){
+// //     parentList[li].innerText = `I am ${li+1} list item.`;
+// // } does not work 
+
+// // for(let i=0; i<parentList.length; i++){
+// //     parentList[i].innerText = `I am ${i+1} list item.\n by JavaScript`;
+// //     parentList[i].style.fontSize = "2rem";
+// //     parentList[i].style.color = "#FDF7C3";
+// //     parentList[i].style.backgroundColor = "#66347F";
+// // }
+
+// // parentList.forEach((element, colorList)=>{
+// // const colorForList = colors[colorList];
+// // console.log(colorForList);
+// // element.innerText = `I am ${element+1} list item.\n by JavaScript`;
+// // element.style.color = colorForList;
+// // element.style.backgroundColor = "cadetblue"
+// //  })  
+
+// // ----------------classList toggle------------------------------
+ 
+// parentList.forEach((element, colorList)=>{
+// element.innerText = `I am list item.\n by JavaScript`;
+// element.classList.toggle('forListItems');
+//  }) 
+
+
+// // -------------CREATE ELEMENT----------------------------------
+
+// const divElement = document.createElement('div');
+// divElement.innerText="I am js generated div"
+// divElement.className = 'forListItems'
+// container.appendChild(divElement);
+
+// // -----------------------------------------------------------
+// // ----------------------------------------------------------
